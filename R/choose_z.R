5d9e8343e150e88f84ab21e6bc0c4f7efbcc5cc5
Jean Morrison <jeanm5@uw.edu>
2016-06-06 12:43:52 -0700

    re-write choose_z function

--- R/choose_z.R
+++ R/choose_z.R
@@ -1,6 +1,24 @@
-
+#' Select threshold value in each segment for a range of lambda values
+#'@description Selects a series of threshold values so that each segment has approximately the same false cluster rate
+#'@param perm.stats p x B matrix of permutation statistics
+#'@param nlam Number of lambda values to consider
+#'@param bw Binwidth for smoothing
+#'@param pos Positions
+#'@param z0 Reference level for merging
+#'@param max.log.lam Maximum value of log(lambda) to consider
+#'@param seg.ends List of segment ends. If NULL assume there is only one segment
+#'@param return.z if FALSE only return maxes list
+#'@param keep.lists If return.z=TRUE also return maxes list
+#'@param except Region to remove when calculating false cluster rate (i.e. signal region)
+#'@param mx Previous maxes list to append
+#' @return If return.z =TRUE, return a matrix that is nlam x (nseg + 1). The first column is log.lambda.
+#' Subsequent columns give the threshold for each segment. If keep.lists=TRUE or return.z=FALSE (also) return
+#' a list of cluster peaks for each segment. mx has length nseg and each element has two columns - peak height and
+#' empirical lambda value.
+#'@export
 choose_z_even <- function(perm.stats, nlam, bw, pos, z0,
-                          lambda.max=NULL, seg.ends=NULL, except=NULL){
+                          max.log.lam=NULL, seg.ends=NULL, return.z=TRUE,
+                          keep.lists=FALSE, except=NULL, mx=NULL){
   if(is.null(seg.ends)){
     seg.ends <- c(length(x))
   }
@@ -10,14 +28,15 @@ choose_z_even <- function(perm.stats, nlam, bw, pos, z0,
     ksmooth(pos, x, bandwidth=bw, x.points=pos)$y
   })
   if(!is.null(except)){
-    for(j in 1:nrow(except)) {
+    for(j in 1:nrow(except)){
       s <- except[j, 1]; p <- except[j, 2]
       perm.smooth[s:p, ] <- 0
     }
   }
 
-  mx <- list()
-  max.lam <- Inf; min.lam <- -Inf; strt <- 1
+  if(is.null(mx)) mx <- list()
+  k <- length(mx)
+  strt <- 1
   for(i in 1:nseg){
     cat(i, " ")
     maxes <- apply(perm.smooth[strt:seg.ends[i],], MARGIN=2, FUN=function(xs){
@@ -28,72 +47,27 @@ choose_z_even <- function(perm.stats, nlam, bw, pos, z0,
       sapply(1:length(starts0), FUN=function(j){ max(abs(xs)[starts0[j]:stops0[j]])})
     })
     m <- sort(unlist(maxes), decreasing=TRUE)
-    mx[[i]] <- cbind(m, (1:length(m))/(nperm*(seg.ends[i]-strt + 1)))
-    max.lam <- min(max.lam, max(log10(mx[[i]][,2])))
-    min.lam <- max(min.lam, min(log10(mx[[i]][,2])))
+    mx[[i+k]] <- cbind(m, (1:length(m))/(nperm*(seg.ends[i]-strt + 1)))
+    max.lam <- min(max.lam, max(log10(mx[[i+k]][,2])))
+    min.lam <- max(min.lam, min(log10(mx[[i+k]][,2])))
     strt <- seg.ends[i] + 1
   }
   cat("\n")
-  if(!is.null(lambda.max)) max.lam <- log10(lambda.max)
+  if(!return.z) return(mx)
 
-  lams <- seq(min.lam, max.lam, length.out=nlam)
+
+  min.log.lam <- sapply(mx, FUN=function(m){min(log10(m[,2]))})
+
+  if(!is.null(max.log.lam)) max.log.lam <- sapply(mx, FUN=function(m){max(log10(m[,2]))})
+
+  lams <- seq(min.log.lam, max.log.lam, length.out=nlam)
   z <- sapply(mx, FUN=function(m){
    approx(y=m[,1], x=log10(m[,2]), xout=lams)$y
   })
   z <- data.frame(cbind(lams, z))
   names(z) <- c("lambda", paste0("z", 1:nseg))
-  return(z)
-}
+  if(! keep.lists) return(z)
 
-
-
-choose_z <- function(lhat, R, level){
-
-  nseg <- ncol(lhat)
-  l <- rowMeans(lhat)
-  r <- rowMeans(R)
-  if(all(l/r > level)){
-    idx <- cbind(rep(Inf, nseg), 1:nseg)
-    f <- lhat/R
-    f[lhat==0 & R ==0] <- Inf
-    if(all(f > level)) return(idx)
-  }else{
-    f <- l/r
-    f[l==0 & r==0] <- Inf
-    m <- max(r[f <= level])
-    ix <- which(r==m & f <= level)
-    idx <- cbind(rep(min(ix), nseg), 1:nseg)
-  }
-  if(nseg==1) return(idx)
-  changed <- rep(1, nseg)
-  j <- 1
-  while(any(changed > 0)){
-    cat(j, "..")
-    inf_ix <- which(!is.finite(idx[,1]))
-    l  <- sum(lhat[idx[-c(inf_ix, j),, drop=FALSE]])
-    r <- sum(R[idx[-c(inf_ix, j),, drop=FALSE]])
-    ll <- l + lhat[,j]
-    rr <- r + R[,j]
-    ff <- ll/rr
-    ff[ll==0 & rr==0] <- Inf
-    if(all(ff > level) & idx[j, 1]==Inf){
-      changed[j] <- 0
-    }else if(sum(ff <= level)==0){ #This only happens for numerical reasons
-      changed[j] <- 0
-    }else{
-      m <- max(rr[ff <= level])
-      ix <- which(rr==m & ff <= level)
-      if(min(ix)==idx[j, 1])changed[j] <- 0
-        else changed[j] <- 1
-      idx[j, 1] <- min(ix)
-    }
-    j <- (j+1) %% nseg
-    if(j==0) j <- nseg
-  }
-  inf_ix <- which(!is.finite(idx[,1]))
-  if(length(inf_ix)==0) r <- sum(R[idx])
-    else r <- sum(R[idx[-c(inf_ix),, drop=FALSE] ])
-  if(r == 0) idx[,1] <- Inf
-  return(idx)
+  return(list("mx"=mx, "z"=z))
 
 }
