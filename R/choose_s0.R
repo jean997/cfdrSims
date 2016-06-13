716e6b963859d866f26d94e9e5a391442df0ea44
Jean Morrison <jeanm5@uw.edu>
2016-06-13 10:55:43 -0700

    new code

---
+++ R/choose_s0.R
@@ -0,0 +1,32 @@
+
+
+choose_s0 <- function(Y, labs, maxit=50){
+
+  x <- cfdrSims:::huber_helper(Y=d$dat[1:2000, ], labs=rep(c(0, 1), d$sample.size), maxit=maxit)
+  salpha <- c(0, as.numeric(quantile(x[2,], probs=seq(0, 1, by=0.05))))
+  nn <- length(salpha)
+  ix <- sapply(x[2,], FUN=function(w){ sum(w >= salpha[-nn])})
+  cvs <- c()
+  v <- as.numeric(by(data=x[1,], INDICES = ix, FUN = mad))
+  cvs <- c(sd(v)/mean(v))
+  tol <- min(x[2,][x[2,] > 0])/10
+
+  fct <- function(s0, Y, labs, ix,  maxit){
+    cat(s0, " ")
+    xx <- cfdrSims:::huber_stats2(Y, labs, s0=s0, maxit=maxit)
+    v <- as.numeric(by(data=xx, INDICES = ix, FUN = mad))
+    return(sd(v)/mean(v))
+  }
+
+  W <- optimize(fct, interval=range(x[2,]), Y=Y,
+                labs=labs, ix=ix, maxit=maxit, tol=tol)
+  return(W$minimum)
+  #for(s in salpha[-1]){
+  #  cat(s, " ")
+  #  xx <- cfdrSims:::huber_stats2(Y, labs, s0=s, maxit=maxit)
+  #  v <- by(data=xx, INDICES = ix, FUN = mad)
+  #  cvs <- c(cvs, sd(v)/mean(v))
+  #}
+  #s=salpha[which.min(cvs)]
+  #return(list("cv"=cvs, "salpha"=salpha, "s"=s, "s0s"=s0))
+}
