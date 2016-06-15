114ddfb2798c582182c3a856696ed707a2d41609
Jean Morrison <jeanm5@uw.edu>
2016-06-14 19:08:21 -0700

    Make more efficient

--- R/choose_s0.R
+++ R/choose_s0.R
@@ -1,33 +1,32 @@
 
 
-choose_s0_z0 <- function(Y, labs, pos, maxit=50, z0_quantile=0.9, bandwidth=20){
-
-  x <- cfdrSims:::huber_helper(Y=Y, labs=labs, maxit=maxit)
-  salpha <- c(0, as.numeric(quantile(x[2,], probs=seq(0, 1, by=0.05))))
+choose_s0 <- function(beta, sds){
+  salpha <- c(0, as.numeric(quantile(sds, probs=seq(0, 1, by=0.05))))
   nn <- length(salpha)
-  ix <- sapply(x[2,], FUN=function(w){ sum(w >= salpha[-nn])})
+  ix <- sapply(sds, FUN=function(w){ sum(w >= salpha[-nn])})
   cvs <- c()
-  v <- as.numeric(by(data=x[1,], INDICES = ix, FUN = mad))
+  v <- as.numeric(by(data=beta/sds, INDICES = ix, FUN = mad, na.rm=TRUE))
   cvs <- c(sd(v)/mean(v))
-  tol <- min(x[2,][x[2,] > 0])/10
+  tol <- max(min(sds[sds > 0]), 1e-3)
 
-  fct <- function(s0, Y, labs, ix,  maxit){
+  fct <- function(s0, beta, sds, ix){
     cat(s0, " ")
-    xx <- cfdrSims:::huber_stats2(Y, labs, s0=s0, maxit=maxit)
-    v <- as.numeric(by(data=xx, INDICES = ix, FUN = mad))
-    return(sd(v)/mean(v))
+    xx <- beta/(sds + s0)
+    v <- as.numeric(by(data=xx, INDICES = ix, FUN = mad, constant = 1/0.64))
+    cv <- sd(v)/mean(v)
+    cat(cv, "\n")
+    return(cv)
   }
 
-  W <- optimize(fct, interval=range(x[2,]), Y=Y,
-                labs=labs, ix=ix, maxit=maxit, tol=tol)
-  xx <- cfdrSims:::huber_stats2(Y, labs, s0=W$minimum, maxit=maxit)
-  xs <- ksmooth(x=pos, y=xx, bandwidth=bandwidth)$y
-  z0 <- as.numeric(quantile(xs, probs=z0_quantile))
-  return(list("s0"=W$minimum, "z0"=z0))
+  W <- optimize(fct, interval=range(sds), beta=beta,
+                sds=sds, ix=ix, tol=tol)
+  return(W$minimum)
 }
 
-dnase_choose_s0_z0 <- function(file.name, pheno.file, seed, has.stats=FALSE, n.select=1e4){
-  set.seed(seed)
+dnase_choose_s0_z0 <- function(file.name, pheno.file,
+                               maxit=50, z0_quantile=0.9,
+                               has.stats=FALSE, tempfile="temp.RData"){
+
   dat <- read_delim(file.name, delim=" ")
   n <- nrow(dat)
   p <- ncol(dat)
@@ -35,10 +34,22 @@ dnase_choose_s0_z0 <- function(file.name, pheno.file, seed, has.stats=FALSE, n.s
     dat <- dat[,-p]
     p <- p-1
   }
-  ix <- sample(1:n, size=n.select, replace=FALSE)
-  dat <- dat[ix,]
+
+
   X <- read_delim(pheno.file, col_names=FALSE, delim=" ")
   x <- as.numeric(X[match(names(dat)[-1], X[,1]),2])
-  s= choose_s0_z0(Y=dat[, -1], labs = x, pos=dat[,1])
-  return(s)
+
+  tt <- huber_helper(dat[, -1], x, maxit=50)
+  miss.ix <- which(is.na(tt[2,]))
+  tt <- tt[, -miss.ix]
+
+  #save(tt, file=tempfile)
+  s= choose_s0(beta=tt[1,],sds=tt[2,])
+
+
+  #xs <- ksmooth(x=dat[,1], y=tt[1,]/(tt[2,] + s), bandwidth=bandwidth, x.points=dat[,1])$y
+  xs <- ksmooth_0(x=dat[-miss.ix,1], y=tt[1,]/(tt[2,] + s), bandwidth=bandwidth)
+  save(tt, xs, file=tempfile)
+  z0 <- as.numeric(quantile(abs(xs), probs=z0_quantile))
+  return(list("sm_stat"=xs, "s0"=s, "z0"=z0))
 }
