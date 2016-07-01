c1da6eae37e08c7dfb2447670986361ed12dc5fd
Jean Morrison <jeanm5@uw.edu>
2016-07-01 14:12:01 -0700

    update window test function

--- R/window_test.R
+++ R/window_test.R
@@ -4,7 +4,7 @@
 #'@param windows q by 2 matrix giving region boundaries
 #'@param type.sequence Vector of integers in 1:6 giving the type of each subregion.
 #'@param dat p x n matrix of data
-#'@param labs 0/1 treatment status of each sample (length n)
+#'@param x treatment status of each sample (length n)
 #'@param s0
 #'@param stat.names Length k. Contains elements of "Poisson", "Huber", "t-test"
 #' @return A list with elements:
@@ -13,36 +13,66 @@
 #' \item{\code{rate_list}}{list of length k. Each element is a three column matrix with columns fdp, tp, fp}
 #' }
 #'@export
-window_test <- function(windows, type.sequence, dat, labs, s0=c(0, 0, 0),
-                                stat.names = c("Poisson", "Huber", "t-test")){
+window_test <- function(windows, dat, pos, x, signal, huber.maxit=50,
+                        s0=c(0, 0, 0), level=c(0.02, 0.05, 0.1, 0.2)){
+
+  stat.names = c("Poisson", "Huber", "t-test")
+  if(all(x %in% c(0, 1))){
+    pois_func = get_stats_pois_binary
+    lm_func <- get_stats_ttest
+  }else{
+    pois_func = get_stats_pois_continuous
+    lm_func=get_stats_lm
+  }
 
   n <- ncol(dat)
   p <- nrow(windows)
-  S <- Intervals(windows)
+  w<- Intervals(windows)
+  if(!class(signal)=="Intervals") signal <- Intervals(signal)
 
+  #Window labels (signal or not)
   l <- rep(0, p)
-  s <- cfdrSims:::get_signal(type.sequence, bandwidth=0)
-  p0 <- nrow(s$signal)
-  d <- distance_to_nearest(S, s$signal)
+  d <- distance_to_nearest(w, signal)
   l[d==0] <- 1
 
-  window.dat <- matrix(nrow=p, ncol=n)
+  #Window data
+  win <- sapply(pos, FUN=function(x){
+      w1 <- which(x >= windows[,1])
+      w2 <- which(x <= windows[,2])
+      wov <- intersect(w1, w2)
+      if(length(wov)==0) return(0)
+        else if (length(wov)==1) return(wov)
+          else return(NA)
+  })
+  dat <- data.frame(dat)
+  dat$win <- win
+  window.dat <- dat %>% group_by(win) %>% summarise_each(funs(sum))
+  window.dat <- window.dat[window.dat$win > 0,]
+
   stats <- qvals <- pvals <- matrix(nrow=length(stat.names), ncol=p)
-  for(i in 1:p){
-    window.dat[i, ] <- colSums(dat[windows[i, 1]:windows[i, 2], ])
-  }
-  rate_list <- list()
+  rates <- array(dim=c(length(stat.names), p, 4))
+  rates_at <- array(dim=c(length(stat.names), length(level), 4 ))
+  dimnames(rates) = list(stat.names, 1:p, c("tpr", "nfp", "ntp", "fdp"))
+  dimnames(rates_at)= list(stat.names, level, c("tpr", "nfp", "ntp", "fdp"))
   for(i in 1:length(stat.names)){
-    if(stat.names[i]=="Poisson") stats[i,] <- pois_regression(Y=window.dat, labs=labs, s0=s0[i])
-    else if(stat.names[i]=="Huber")  stats[i, ] <- cfdrSims:::huber_stats2(Y=window.dat, labs=labs, s0=s0[i])
-    else if(stat.names[i]=="t-test") stats[i, ]<- cfdrSims:::t_stats(window.dat, labs=labs, s0=s0[i])
-    rate_list[[i]] <- data.frame(t(sapply(sort(abs(stats[i,])), FUN=function(x){
-      tpr_nfp(s$signal, discoveries=S[abs(stats[i,]) >= x, , drop=FALSE])
-    })))
+    if(stat.names[i]=="Poisson"){
+      stats[i,] <- pois_func(window.dat[,-1], x, NULL, s0=s0[i])
+    }else if(stat.names[i]=="Huber"){
+      stats[i,]<- huber_stats2(window.dat[,-1], x, s0=s0[i], maxit=huber.maxit)
+    }else if(stat.names[i]=="t-test"){
+      stats[i,] <- lm_func(window.dat[,-1], x, NULL, s0=s0[i])
+    }
+    rates[i, , ] <- t(sapply(sort(abs(stats[i,])), FUN=function(xx){
+      tpr_nfp(signal, discoveries=w[abs(stats[i,]) >= xx, , drop=FALSE])
+    }))
     pvals[i,] <- sapply(stats[i,], FUN=cfdrSims:::get.pt, df=n-2)
     qvals[i,] <- p.adjust(pvals[i,], method="BH")
+
+    rates_at[i, , ] <- t(sapply(level, FUN=function(ll){
+      cfdrSims:::tpr_nfp(signal=signal, discoveries = w[qvals[i, ] <= ll, ])
+    }))
   }
-  return(list("stats"=stats, "rate_list"=rate_list,
+  return(list("stats"=stats, "rates"=rates, "rates_at"=rates_at, "windows"=windows,
               "stat.names"=stat.names, "pvals"=pvals, "qvals"=qvals, "class"=l))
 
 
