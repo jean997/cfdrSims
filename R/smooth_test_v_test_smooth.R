894bde5ada7b6ac812e702756a045e74e772cfaa
Jean Morrison <jeanm5@uw.edu>
2016-07-25 10:48:18 -0700

    add option for splines

--- R/smooth_test_v_test_smooth.R
+++ R/smooth_test_v_test_smooth.R
@@ -1,15 +1,23 @@
 
 #All have t-test and box kernel smoother
 #'@export
-test_smooth_jade_sim <- function(data.file, profiles, seed=NULL, bandwidth=20){
+test_smooth_jade_sim <- function(data.file, profiles, bandwidth=20, smooth.type="box"){
 
-
-  if(!is.null(seed)) set.seed(seed)
 
   f <- getobj(data.file)
   labs <- rep(c(0, 1), f$sample.size)
   n <- nrow(f$Y)
 
+  smooth.type <- match.arg(smooth.type, c("box", "spline"))
+  if(smooth.type==box){
+    smooth.func <- function(y){
+      ksmooth(x=1:n, y=y, x.points = 1:n, bandwidth=bandwidth)$y
+    }
+  }else if(smooth.type=="spline"){
+    smooth.func <- function(y){
+      smooth.spline(x=1:n, y=y)$y
+    }
+  }
 
   #Build signal intervals object
   q0 <-rle( abs(profiles[,1]-profiles[,2]) > 1e-6 )
@@ -21,17 +29,23 @@ test_smooth_jade_sim <- function(data.file, profiles, seed=NULL, bandwidth=20){
   pw_signal[ abs(profiles[,1]-profiles[,2]) > 1e-6 ] <- 1
 
   #Stats and smoothed stats
-  stats <- t_stats(dat=f$Y, labs=labs, s0=0)
-  ys <- ksmooth(x=1:n, y=stats, bandwidth=bandwidth, x.points=1:n)$y
+  stats <- cfdrSims:::t_stats(dat=f$Y, labs=labs, s0=0)
+  ys <- smooth.func(stats)
 
   #Smoothed data and stats
   Ys <- apply(f$Y, MARGIN=2, FUN=function(y){
-    ksmooth(x=1:n, y=y, bandwidth = bandwidth, x.points=1:n)$y
+    smooth.func(y)
   })
   stat_Ys <- cfdrSims:::t_stats(dat=Ys, labs=labs, s0=0)
 
   #Rates for test then smooth
-  yy <- sort(abs(ys), decreasing=TRUE)[1:(0.5*n)]
+  yy <- sort(abs(ys), decreasing=TRUE)
+  pw_ts <- data.frame(t(sapply(yy, FUN=function(x){
+    sep <- as.numeric(abs(ys) > x)
+    cbind(tpr.func(sep, pw_signal), fpr.func(sep, pw_signal))
+  })))
+  names(pw_ts) <- c("tpr", "fpr")
+  yy <- yy[1:(0.5*n)]
   rw_ts <- data.frame(t(sapply(yy, FUN=function(x){
       cfdrSims:::tpr_nfp(signal, x=ys, z = x, z0=x)
       #z0=x means no merging
@@ -39,14 +53,16 @@ test_smooth_jade_sim <- function(data.file, profiles, seed=NULL, bandwidth=20){
   rw_ts_merge <- data.frame(t(sapply(yy, FUN=function(x){
     cfdrSims:::tpr_nfp(signal, x=ys, z = x, z0=yy[length(yy)])
   })))
-  pw_ts <- data.frame(t(sapply(yy, FUN=function(x){
-    sep <- as.numeric(abs(ys) > x)
-    cbind(tpr.func(sep, pw_signal), fpr.func(sep, pw_signal))
-  })))
-  names(pw_ts) <- c("tpr", "fpr")
+
 
   #Rates for smooth then test
-  yy <- sort(abs(stat_Ys), decreasing=TRUE)[1:(0.5*n)]
+  yy <- sort(abs(stat_Ys), decreasing=TRUE)
+  pw_st <- data.frame(t(sapply(yy, FUN=function(x){
+    sep <- as.numeric(abs(stat_Ys) > x)
+    cbind(tpr.func(sep, pw_signal), fpr.func(sep, pw_signal))
+  })))
+  names(pw_st) <- c("tpr", "fpr")
+  yy <- yy[1:(0.5*n)]
   rw_st <- data.frame(t(sapply(yy, FUN=function(x){
     cfdrSims:::tpr_nfp(signal, x=stat_Ys, z = x, z0=x)
     #z0=x means no merging
@@ -54,12 +70,6 @@ test_smooth_jade_sim <- function(data.file, profiles, seed=NULL, bandwidth=20){
   rw_st_merge <- data.frame(t(sapply(yy, FUN=function(x){
     cfdrSims:::tpr_nfp(signal, x=stat_Ys, z = x, z0=yy[length(yy)])
   })))
-  pw_st <- data.frame(t(sapply(yy, FUN=function(x){
-    sep <- as.numeric(abs(stat_Ys) > x)
-    cbind(tpr.func(sep, pw_signal), fpr.func(sep, pw_signal))
-  })))
-  names(pw_st) <- c("tpr", "fpr")
-
 
 
   R <- list("ys"=ys, "rw_ts"=rw_ts,
